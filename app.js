423be74ce7a8f7c801c0e76a0599399fba77e1d5
jacopo1395 <jack1395@hotmail.it>
2017-02-17 18:31:13 +0100

    bug fix

--- app.js
+++ app.js
@@ -73,8 +73,7 @@ app.get('/info/:id', function (req, res) {
             data.genres = info.genres;
             mdb.movieCredits({id: req.params.id}, function (err, cred) {
                 if (err) throw err;
-                data.cred=cred;
-                console.log(data.cred.cast);
+                data.cred = cred;
                 res.render('movie', {'data': data});
             });
         });
@@ -90,8 +89,8 @@ app.get('/genres', function (req, res) {
 
 
 //GET /link Return
-app.get('/link/', function (req, res) {
-    console.log('/link ' + req.query.q);
+app.get('/player/', function (req, res) {
+    console.log('/player ' + req.query.q);
     var file_torrent;
     icn.getMagnet(req.query.q, function (err, data) {
         if (err) throw err;
@@ -117,11 +116,11 @@ app.get('/link/', function (req, res) {
                 file_torrent.on('done', function () {
                     console.log('done');
                 });
-                file_torrent.on('download', function () {
+                file_torrent.on('download', function (bytes) {
                     console.log('download');
                 });
                 console.log('render');
-                res.render('vlc', {'data': file_torrent.path});
+                res.render('player', {'data': file_torrent.path});
             });
             client.on('torrent', function (torrent) {
                 console.log("torrent");
@@ -130,13 +129,89 @@ app.get('/link/', function (req, res) {
 
             client.on('error', function (err) {
                 console.log(err);
-                res.render('vlc');
+                res.send('error');
             });
         }
     });
 
 });
 
+app.get('/play3', function (req, res) {
+    console.log('/play');
+    //console.log(req.query.q);
+    var p = req.query.q;
+    var type = p.substring(p.length - 3, p.length);
+    // console.log(type);
+    var file = path.resolve(__dirname + '/download', p);
+    fs.stat(file, function (err, stats) {
+        if (err) {
+            if (err.code === 'ENOENT') {
+                // 404 Error if file not found
+                return res.sendStatus(404);
+            }
+            res.end(err);
+        }
+        var range = req.headers.range;
+        if (!range) {
+            // 416 Wrong range
+            return res.sendStatus(416);
+        }
+        var positions = range.replace(/bytes=/, "").split("-");
+        var start = parseInt(positions[0], 10);
+        var total = stats.size;
+        var end = positions[1] ? parseInt(positions[1], 10) : total - 1;
+        var chunksize = (end - start) + 1;
+
+
+        // res.writeHead(200, {
+        //     "Content-Range": "bytes " + start + "-" + end + "/" + total,
+        //     "Accept-Ranges": "bytes",
+        //     "Content-Length": chunksize,
+        //     "Content-Type": "video/" + type
+        // });
+        res.contentType('mp4');
+
+        var stream = ((client.torrents[0].files[0]).createReadStream({start: start, end: end}));
+
+        var input_file = stream;
+        var process = child_process.spawn('ffmpeg', ['-i', 'pipe:0', '-f', 'mp4', '-movflags', 'frag_keyframe', 'pipe:1']);
+        input_file.pipe(process.stdin);
+        process.stdout.pipe(res);
+        console.log('fine test');
+
+
+    });
+
+
+});
+
+app.get('/play2', function (req, res) {
+    res.contentType('/play2');
+    // make sure you set the correct path to your video file storage
+    var p = req.query.q;
+    var type = p.substring(p.length - 3, p.length);
+    // console.log(type);
+    var file = path.resolve(__dirname + '/download', p);
+    var input_file = fs.createReadStream(file);
+    var process = child_process.spawn('ffmpeg', ['-i', 'pipe:0', '-f', 'mp4', '-movflags', 'frag_keyframe', 'pipe:1']);
+    input_file.pipe(process.stdin);
+    process.stdout.pipe(res);
+});
+
+app.get('/play', function(req,res){
+    var p = req.query.q;
+    var type = p.substring(p.length - 3, p.length);
+    // console.log(type);
+    var file = path.resolve(__dirname + '/download', p);
+    var stream = ((client.torrents[0].files[0]).createReadStream());
+    var command = ffmpeg(file);
+    res.contentType('mp4');
+    command.output(__dirname +'/download/outputfile.mp4').run();
+    var out = ffmpeg(__dirname +'/download/outputfile.mp4');
+    out.output(res).run();
+
+
+});
 
 var containsMagnet = function (elem, array) {
     for (var i in array) {
